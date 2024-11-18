915ba0b9d55832d2e8bc2c4a6885b6f5d18ea1ef
Gimasss <146975085+Gimasss@users.noreply.github.com>
2024-11-18 16:51:36 +0100

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -17,7 +17,14 @@ The function takes three arguments:
 	int c: This is the value to be set. The value is passed as an int, but the 
 		function fills the block of memory using the unsigned char conversion 
 		of this value. (SO i have to make a cast)
-	size_t n: This is the number of bytes to be set to the value.
+
+  	more about this: Using unsigned char in the ft_memset function is necessary because ft_memset is typically used to set a block of memory to a specific byte value. 
+   	Here are the key reasons:
+	Range of Values: An unsigned char can hold values from 0 to 255, whereas a signed char can hold values from -128 to 127. ft_memset is often used to set bytes to any value in the 0-255 range, making unsigned char the appropriate type to handle the full range of possible byte values.
+	Consistency with Standard Library: The standard library function memset in C also uses unsigned char for the value to set, ensuring consistency with standard practices.
+	Avoid Negative Values: Using unsigned char prevents negative values from being interpreted incorrectly when setting memory.
+	
+ size_t n: This is the number of bytes to be set to the value.
 The function returns a pointer to the memory area `s`.
 */
 #include "libft.h"
